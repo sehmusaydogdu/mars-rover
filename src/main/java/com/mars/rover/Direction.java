80cc61d62a471e412098caacf9f6981922d494d9
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2021-10-23 22:26:35 +0300

    direction class added.

---
+++ src/main/java/com/mars/rover/Direction.java
@@ -0,0 +1,22 @@
+package com.mars.rover;
+
+/**
+ * Compass keeps direction.
+ * 
+ * @author Şeyhmus Aydoğdu
+ *
+ */
+public enum Direction {
+
+	/** NORT */
+	N,
+
+	/** SOUTH */
+	S,
+
+	/** EAST */
+	E,
+
+	/** WEST */
+	W
+}
